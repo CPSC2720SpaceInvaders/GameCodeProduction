87eea1ac69fc3f35a4ac8c3c5fd935924eeac971
VictorAdad <adad.v95@gmail.com>
2016-03-14 17:49:48 -0600

    We need to be able to compile

--- main.cpp
+++ main.cpp
@@ -1,16 +1,15 @@
 #include <allegro.h>
+// we need to be able to compile, everyone of us, this file on our own computer
 
 int main(){
-
+ 
     allegro_init();
     install_keyboard();
     set_gfx_mode( GFX_AUTODETECT, 640, 480, 0, 0);
-
+    
     readkey();
-
+    
     return 0;
-
-}
-END_OF_MAIN();
-
-//Hola
+    
+}   
+END_OF_MAIN(); 

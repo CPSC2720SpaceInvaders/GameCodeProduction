662c0c9fd9ba736ba1559539d55476667de2ac12
VictorAdad <adad.v95@gmail.com>
2016-03-14 18:54:56 -0600

    Main updated to Allegro version 5.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,28 @@
-#include <allegro.h>
-// we need to be able to compile, everyone of us, this file on our own computer
+#include <stdio.h>
+#include <allegro5/allegro.h>
 
-int main(){
- 
-    allegro_init();
-    install_keyboard();
-    set_gfx_mode( GFX_AUTODETECT, 640, 480, 0, 0);
-    
-    readkey();
-    
-    return 0;
-    
-}   
-END_OF_MAIN(); 
+int main(int argc, char **argv){
+
+   ALLEGRO_DISPLAY *display = NULL;
+
+   if(!al_init()) {
+      fprintf(stderr, "failed to initialize allegro!\n");
+      return -1;
+   }
+
+   display = al_create_display(640, 480);
+   if(!display) {
+      fprintf(stderr, "failed to create display!\n");
+      return -1;
+   }
+
+   al_clear_to_color(al_map_rgb(0,0,0));
+
+   //al_flip_display();
+
+   al_rest(10.0);
+
+   al_destroy_display(display);
+
+   return 0;
+}

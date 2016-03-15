2822dfd12e3b3dff041bbfc1a707214031a71716
VictorAdad <adad.v95@gmail.com>
2016-03-15 02:13:04 -0600

    Allegro draws a circle and the user is able to move it using keyboard

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <allegro5/allegro_native_dialog.h>
 #include <allegro5/allegro_ttf.h>
 #include <allegro5/allegro_font.h>
+#include <allegro5/allegro_primitives.h>
+#include <iostream>
 
 #define ScreenWidth 800
 #define ScreenHeight 600
@@ -15,7 +17,7 @@ int main(){
    }
 
    al_set_new_display_flags(ALLEGRO_NOFRAME);
-   ALLEGRO_DISPLAY *display = al_create_display(800, 600);
+   ALLEGRO_DISPLAY *display = al_create_display(ScreenWidth, ScreenHeight);
    al_set_window_position(display, 200, 100);
    al_set_window_title(display, "2720");
 
@@ -24,13 +26,59 @@ int main(){
       return -1;
    }
 
-   al_init_font_addon();
+   al_init_font_addon(); //intializes fonts so we can write on the display
    al_init_ttf_addon();
    ALLEGRO_FONT *font = al_load_font("Custom Font.ttf", 36, NULL);
    al_draw_text(font, al_map_rgb(44,117,255), ScreenWidth/2, ScreenHeight/2, ALLEGRO_ALIGN_CENTRE, "Hello, 2720 team!");
-   al_flip_display();
-   al_rest(5.0);
+
+   al_init_primitives_addon();  //intializes primitives to draw figures
+   ALLEGRO_COLOR electricBlue = al_map_rgb(44,117,255);
+//   float points[8] = {0, 0, 400, 100, 50, 200, ScreenWidth, ScreenHeight};
+//   al_draw_spline(points, electricBlue, 1.0);
+   al_draw_filled_triangle(10, 10, 40, 10, 25, 40, al_map_rgb(200,0,0)); // draws like: X1,Y1, X2,Y2, X3,Y3
+   al_draw_rectangle(100, 100, 150, 130, al_map_rgb(0,200,0), 6.0);
+   //al_draw_filled_circle(ScreenWidth/2, ScreenHeight-100, 10, electricBlue);
+   al_draw_arc(10, ScreenHeight-100, 10, 0, 4.0, al_map_rgb(255,0,240), 3.0);
+   al_draw_line(100, 500, 300, 500, electricBlue, 6.0);
+   al_draw_pixel(500, 500, electricBlue);
+
+   al_install_keyboard();  //intializes the hability to recieve commands from keyboard
+   ALLEGRO_EVENT_QUEUE *event_queue1 = al_create_event_queue();
+   al_register_event_source(event_queue1, al_get_keyboard_event_source());
+   bool done = false;
+   int x=10, y=10;
+   int moveSpeed = 5;
+   //int state = NULL;
+   while(!done){
+        ALLEGRO_EVENT events;
+        al_wait_for_event(event_queue1, &events); //waits for something to happen
+        if (events.type == ALLEGRO_EVENT_KEY_DOWN){
+            switch(events.keyboard.keycode){
+                case ALLEGRO_KEY_DOWN:
+                    y = y + moveSpeed;
+                    break;
+                case ALLEGRO_KEY_UP:
+                    y = y - moveSpeed;
+                    break;
+                case ALLEGRO_KEY_RIGHT:
+                    x = x + moveSpeed;
+                    break;
+                case ALLEGRO_KEY_LEFT:
+                    x = x - moveSpeed;
+                    break;
+                case ALLEGRO_KEY_ESCAPE:
+                    done = true;
+                    break;
+            }
+        }
+        al_draw_filled_circle(x, y, 10, electricBlue); //draws a circle everytime a key is pressed
+        al_flip_display();
+        al_clear_to_color(al_map_rgb(0,0,0)); //cleans screen so it looks like moving
+   }
+
+   //al_rest(5.0); //number of seconds the program waits before closing itself
    al_destroy_display(display);
+   al_destroy_event_queue(event_queue1);
    return 0;
 }
 

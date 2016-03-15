ae86f6babfcf86a7e1f62eef786d6f0e41f561ca
VictorAdad <adad.v95@gmail.com>
2016-03-15 00:48:57 -0600

    Program prints on screen Hello, 2720 team!

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 #include <allegro5/allegro_ttf.h>
 #include <allegro5/allegro_font.h>
 
+#define ScreenWidth 800
+#define ScreenHeight 600
+
 int main(){
 
    if(!al_init()) {
@@ -11,7 +14,7 @@ int main(){
       return -1;
    }
 
-   //al_set_new_display_flags();
+   al_set_new_display_flags(ALLEGRO_NOFRAME);
    ALLEGRO_DISPLAY *display = al_create_display(800, 600);
    al_set_window_position(display, 200, 100);
    al_set_window_title(display, "2720");
@@ -21,9 +24,14 @@ int main(){
       return -1;
    }
 
-   //al_show_native_message_box(display, "Message Title", "Display Settings", "Display window is done", NULL, NULL);
-
+   al_init_font_addon();
+   al_init_ttf_addon();
+   ALLEGRO_FONT *font = al_load_font("Custom Font.ttf", 36, NULL);
+   al_draw_text(font, al_map_rgb(44,117,255), ScreenWidth/2, ScreenHeight/2, ALLEGRO_ALIGN_CENTRE, "Hello, 2720 team!");
+   al_flip_display();
    al_rest(5.0);
    al_destroy_display(display);
    return 0;
 }
+
+//al_show_native_message_box(display, "Message Title", "Display Settings", "Display window is done", NULL, NULL);

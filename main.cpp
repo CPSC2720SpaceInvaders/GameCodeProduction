007aa105261acbf65b1264fab844bdd39503f04b
VictorAdad <adad.v95@gmail.com>
2016-03-14 23:51:01 -0600

    Modification of window properties

--- main.cpp
+++ main.cpp
@@ -1,28 +1,29 @@
 #include <stdio.h>
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_native_dialog.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_font.h>
 
-int main(int argc, char **argv){
-
-   ALLEGRO_DISPLAY *display = NULL;
+int main(){
 
    if(!al_init()) {
-      fprintf(stderr, "failed to initialize allegro!\n");
+      al_show_native_message_box(NULL, NULL, NULL, "Failed to initialize allegro 5!", NULL, NULL);
       return -1;
    }
 
-   display = al_create_display(640, 480);
+   //al_set_new_display_flags();
+   ALLEGRO_DISPLAY *display = al_create_display(800, 600);
+   al_set_window_position(display, 200, 100);
+   al_set_window_title(display, "2720");
+
    if(!display) {
-      fprintf(stderr, "failed to create display!\n");
+      al_show_native_message_box(display, "Error Title", "Display Settings", "Display window was unsuccessfull", NULL, NULL);
       return -1;
    }
 
-   al_clear_to_color(al_map_rgb(0,0,0));
+   //al_show_native_message_box(display, "Message Title", "Display Settings", "Display window is done", NULL, NULL);
 
-   //al_flip_display();
-
-   al_rest(10.0);
-
+   al_rest(5.0);
    al_destroy_display(display);
-
    return 0;
 }

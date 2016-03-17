23df8c5905d54d513322550a3c027c70ffcdefa9
TheRadEcks <themostradicalX@gmail.com>
2016-03-17 01:29:31 -0600

    formatting projectile class for readability, doxygen comment support

--- projectile.cpp
+++ projectile.cpp
@@ -1,4 +1,15 @@
-//Header comments
+/**		
+*		@file	projectile.cpp
+*		@brief	Implementation of projectile class.
+*		
+*				This contains the implementation for the methods
+*				of the Projectile class, which handles
+*				the vectors of interaction between actors
+*				in the game.
+*				
+*		@author	Tyler Bertram
+*		@bug	No known bugs (uncompiled, untested).
+*/
 
 //includes
 #include "projectile.h"
@@ -6,17 +17,27 @@
 //Private methods
 
 //Public methods
-int getXCoord(){
+int GetXCoord(){
    return xCoordinate;
-}int getYCoord(){
+}int GetYCoord(){
    return yCoordinate;  
-}void moveProjectile (int moveRate, Directions_c projDirection){
+}
+
+/**
+*	Additional comments about MoveProjectile method.
+*/
+void MoveProjectile (int moveRate, Directions_c projDirection){
    if (projDirection == UP){
       yCoordinate -= moveRate;
    }else{
       yCoordinate += moveRate;
    }
-}bool checkForCollission(int targetXCoord, int targetYCoord){
+}
+
+/**
+*	Additional comments about CheckForCollission method.
+*/
+bool CheckForCollision(int moveRate, int targetXCoord, int targetYCoord){
    if (xCoordinate == targetXCoord && yCoordinate == targetYCoord){
       return true;
    }return false;
--- projectile.h
+++ projectile.h
@@ -1,25 +1,61 @@
-//Header comments
+/**
+*		@file	projectile.h
+*		@brief	Definition of projectile class.
+*
+*				This contains the definitions of the public and
+*				private member variables, as well as all methods,
+*				for the Projectile class, which handles
+*				interactions between actors in the game.
+*
+*		@author	Tyler Bertram
+*		@bug	No known bugs (uncompiled, untested).
+*/
 
 #ifndef PROJECTILE_H__
 #define PROJECTILE_H__
 
-//includes
 #include <allegro5/allegro.h>
 
 class Projectile {
-  Private:
-   //Member variables
-   int xCoordinate, yCoordinate;
-   enum Directions_c (UP, DOWN);
-   
-  Public:
+Private:
+	//Member variables
+	int		xCoordinate, yCoordinate; /**< Determines location of projectile at a given time. */
+	enum	directions_c { UP, DOWN }; /**< Determines whether a fired projectile should travel towards the top or bottom of the screen. */
+
+ Public:
    //Retrieval methods
-   int getXCoord();
-   int getYCoord();
+	/** 
+	*		@fn			GetXCoord()
+	*		@brief		Exposes and returns xCoordinate.
+	*		@return		integer value.
+	*/
+   int		GetXCoord();
+   /**
+   *		@fn			GetYCoord()
+   *		@brief		Exposes and returns yCoordinate.
+   *		@return		Integer value.
+   */
+   int		GetYCoord();
 
    //Manipulation methods
-   void moveProjectile(int moveRate, Directions_c projDirection);
-   bool checkForCollission(int targetXCoord, int targetYCoord);
-}
+   /** 
+   *		@fn			MoveProjectile( int moveRate, directions_c projDirection )
+   *		@brief		Moves the single-pixel location of a projectile a given 
+   *					distance down or up. Modifies xCoordinate and yCoordinate.
+   *		@param		moveRate		Rate of movement for the projectile (in px).
+   *		@param		projDirection	Direction of movement for projectile (up/down).
+   */
+   void		MoveProjectile( int moveRate, directions_c projDirection );
+   /** 
+   *		@fn			CheckForCollission( int moveRate, int targetXCoord, int targetYCoord )
+   *		@brief		Checks along a specified movement path for a collision between
+   *					a projectile and an actor.
+   *		@param		moveRate		Vertical rate of movement for projectile.
+   *		@param		targetXCoord	Location of actor, horizontally.
+   *		@param		targetYCoord	Location of actor, vertically.
+   *		@return		Boolian value.
+   */
+   bool		CheckForCollision( int moveRate, int targetXCoord, int targetYCoord );
+};
 
 #endif //Projectile.h
